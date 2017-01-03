dffb437add620a45c6dd06e7c316783a277bb5ad
Patrick Balsiger <frupii@gmail.com>
2017-01-03 02:04:29 +0100

    fix build

---
+++ dist/config/feeds.js
@@ -0,0 +1,6 @@
+module.exports = {
+    checkFeedInterval: '*/10 * * * * *',
+    feeds: [
+        'http://showrss.info/user/76295.rss?magnets=true&namespaces=true&name=null&quality=null&re=null'
+    ]
+};
---
+++ dist/config/rtorrent.js
@@ -0,0 +1,8 @@
+module.exports = {
+	mode: 'xmlrpc',
+	host: '192.168.1.121',
+	port: 80,
+	path: '/RPC2',
+	user: 'user',
+	pass: 'password'
+};
