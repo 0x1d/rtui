4f3a735412e911115bf70827cc82c300416b0251
Patrick Balsiger <frupii@gmail.com>
2017-03-18 02:51:30 +0100

    fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,20 @@
 module.exports = {
-  entry: './src/website/scripts/main.js',
-  output: {
-    filename: './dist/website/scripts.js'
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015'],
-          plugins: ['transform-runtime']
-        }
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['', '.js', '.json']
-  }
+    entry: './src/website/scripts/main.js',
+    output: {
+        filename: './dist/website/scripts.js'
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            exclude: /(node_modules|bower_components)/,
+            loader: 'babel-loader',
+            query: {
+                presets: ['es2015'],
+                plugins: ['transform-runtime']
+            }
+        }]
+    },
+    resolve: {
+        extensions: ['.js', '.json']
+    }
 };
